9d3cb7d8aaedcb0bc2bb3e776f14d65d35f24b74
Telehakke <nimaruitigo@gmail.com>
2024-10-17 21:02:39 +0900

    Resolve security issues

--- src/models/formattedHTML.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from "vitest";
-import formattedHTML from "./formattedHTML";
-
-test("htmlの2行目が字下げされた、新しいHTMLが取得できるかどうか", () => {
-    const html = "　Line1<br>\nLine2";
-    const editorLines = ["　Line1", "　Line2"];
-    const result = formattedHTML(html, editorLines);
-    expect(result).toBe("　Line1<br>\n　Line2");
-});
--- vite.config.js
+++ vite.config.js
@@ -13,4 +13,7 @@ export default defineConfig({
             external: ["obsidian"],
         },
     },
+    test: {
+        environment: "jsdom",
+    },
 });
